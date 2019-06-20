1c6ea80207b9d85de137780c5c58540939e63e33
jalapina <davidpina14@gmail.com>
2019-06-20 16:50:34 -0500

    Added Doughballs to shoowcase and updated weather app gif

---
+++ davidpina/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    var form_id = "contact-form";
+    
+        var data = {
+            "access_token": "bc8723fc6gdlfyhufp4g2izv"
+        };
+    
+        var sendButton = $("#" + form_id + " [name='send']");
+    
+        function send() {
+            sendButton.val('Sending…');
+            sendButton.prop('disabled',true);
+
+            var subject = $("#" + form_id + " [name='subject']").val();
+            var message = $("#" + form_id + " [name='message']").val();
+            
+            data['subject'] = subject;
+            data['text'] = message;
+
+            $.post('https://postmail.invotes.com/send',data, function(data){
+                sendButton.val('Sent');
+            })
+            return false;
+        }
+    
+        sendButton.on('click', send);
+    
+        var $form = $("#" + form_id);
+
+        $form.submit(function( event ) {
+            sendButton.prop('disabled',false);
+            event.preventDefault();
+            
+        });
+});
